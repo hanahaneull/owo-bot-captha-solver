f31b1789cdee59601cdf5aa6ddd903b1da53fc09
hanahaneull <48522963+hanahaneull@users.noreply.github.com>
2019-12-29 04:21:52 +0700

    better version

--- owo.js
+++ owo.js
@@ -1,32 +1,56 @@
-var Antigate = require('antigate');
-var ag = new Antigate('ya yeet');
+const image2base64 = require('image-to-base64');
+const Anticaptcha = require('anticaptcha2');
+const anticaptcha = new Anticaptcha('api-key');
+const ImageToTextTask = Anticaptcha.ImageToTextTask;
 const discord = require('discord.js');
 const client = new discord.Client();
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
-client.on("ready", () => {
-	console.log(`${client.user.tag}`);
+
+client.on("ready", async () => {
+	var saldo = await anticaptcha.getBalance();
+	console.log(`${client.user.tag}, Bring it on!`);
+	console.log(saldo);
 });
 
-client.on("message", async msg => {
-
-	if (msg.author.id === "408785106942164992") {
-		if (msg.content.includes("Are you a real human?")){
-		let mAuthor = msg.author.lastMessageID;
-		let message_id = msg.channel.messages.get(`${mAuthor}`);
-		let gAttachment = message_id.attachments.map(file => file.id);
-		let attachment_id = message_id.attachments.get(`${gAttachment}`);
-		let attached = `${attachment_id.url}`;
-		console.log(attached);
-		ag.processFromURL(attached, function(error, text, id) {
-			  if (error) {
-    			throw error;
-  				} else {
-  					console.log(text);
-  					msg.channel.send(text);}});
+client.on("message", async (msg) => {
+	if (msg.author.id === "658605529157074954") {
+		if (msg.content.includes("Are you a real human?")) {
+			var link;
+			var mAuthor = msg.author.lastMessageID;
+			var message_id = msg.channel.messages.get(`${mAuthor}`);
+			var gAttachment = message_id.attachments.map(file => file.id);
+			var attachment_id = message_id.attachments.get(`${gAttachment}`);
+			var attached = `${attachment_id.url}`;
+			link = attached;
+			var base64 = await image2base64(attached);
+			var captha = new ImageToTextTask({
+				body: base64,
+				phrase: false,
+				case: true,
+				numeric: 0,
+				math: false,
+				length: [5,5]
+			});
+			var selesai = await anticaptcha.solve(captha);
+			msg.channel.send(selesai.text)
+				.then(() => {
+  						msg.channel.awaitMessages(response => response.content.includes("I have verified that you are human"), {
+    						max: 1,
+    						time: 30000,
+    						errors: ['time'],
+  						})
+  					.then((collected) => {
+      					console.log("Mantap");
+    						})
+    					.catch(() => {
+     						console.log("Gk mantap");
+    					});
+					});
 		};
-
 	};
 });
 
-client.login("djiancox");
+
+
+client.login("dicsordasdawdawdawd");
